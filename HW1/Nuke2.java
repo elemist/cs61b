a76ea93d8fc7e48b9e8a54f8316b3f15a8f805db
elemist <yuanyuannpu@gmail.com>
2017-09-08 11:53:10 -0400

    Add files via upload

---
+++ HW1/Nuke2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Nuke2 {
+    public static void main(String arg[]) throws Exception {
+    BufferedReader keyboard;
+    String inputLine;
+    keyboard = new BufferedReader(new InputStreamReader(System.in));
+    inputLine = keyboard.readLine();
+    System.out.println(inputLine.substring(0,1) + inputLine.substring(2,inputLine.length()));       
+    }
+}
